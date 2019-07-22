1613f52d6bc3f006958661a0f2961c837d4f0356
Ricardo Colares <ricardo.colares@osf-global.com>
2019-07-21 21:12:19 -0300

    envio de email conclido

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5a61315d2f117d',
+    pass: 'c33025b5c840c5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
